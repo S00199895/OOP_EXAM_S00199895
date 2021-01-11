8c43061230187d7376a860fba8cd40c2d6ebc7fa
Mark Curran <S00199895@mail.itsligo.ie>
2021-01-11 09:43:04 +0000

    Created abstract class Account.cs

---
+++ Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    //abstract class. cannot be implemented oly inherited from
+    public abstract class Account
+    {
+        //properties
+        public abstract string FirstName { get; set; }
+        public abstract string LastName { get; set; }
+        public abstract decimal Balance { get; set; }
+        public abstract decimal InterestRate { get; set; }
+
+        //methods
+        public abstract void Deposit();
+        public abstract void Withdraw();
+
+    }
+}
