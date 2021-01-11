f030d9fc5f9647a44af45b20d4eae4005863aa26
Mark Curran <S00199895@mail.itsligo.ie>
2021-01-11 09:49:50 +0000

    Implemented Savings and Current accounts

--- Account.cs
+++ Account.cs
@@ -13,11 +13,62 @@ namespace WpfApp1
         public abstract string FirstName { get; set; }
         public abstract string LastName { get; set; }
         public abstract decimal Balance { get; set; }
-        public abstract decimal InterestRate { get; set; }
+        public abstract decimal InterestDate { get; set; }
 
         //methods
-        public abstract void Deposit();
-        public abstract void Withdraw();
+        public void Deposit()
+        {
 
+        }
+
+        public void Withdraw()
+        {
+
+        }
+
+        public abstract decimal CalculateInterest();
+    }
+
+    public class CurrentAccount : Account
+    {
+        //props
+        public override string FirstName { get; set; }
+        public override string LastName { get; set; }
+        public override decimal Balance { get; set; }
+        public override decimal InterestDate { get; set; }
+        public decimal InterestRate { get; set; }
+        
+        //ctors
+        public CurrentAccount()
+        {
+            InterestRate = .03m;
+        }
+
+        //methods
+        public override decimal CalculateInterest()
+        {
+            return 0;
+        }
+    }
+    public class SavingsAccount : Account
+    {
+        //props
+        public override string FirstName { get; set; }
+        public override string LastName { get; set; }
+        public override decimal Balance { get; set; }
+        public override decimal InterestDate { get; set; }
+        public decimal InterestRate { get; set; }
+
+        //ctors
+        public SavingsAccount()
+        {
+            InterestRate = .06m;
+        }
+
+        //methods
+        public override decimal CalculateInterest()
+        {
+            return 0;
+        }
     }
 }
